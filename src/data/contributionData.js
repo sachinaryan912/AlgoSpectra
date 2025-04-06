30919c4c153c0be29ddbc1f9b6a7c467ad8710da
Sachin kumar <sachinsachu1998.ss@gmail.com>
2025-04-06 08:51:07 +0530

    added contributors

---
+++ src/data/contributionData.js
@@ -0,0 +1,14 @@
+export const contributors = [
+    {
+      name: "Sachin Kumar",
+      role: "Software Engineer",
+      avatar: "https://avatars.githubusercontent.com/u/67205331?v=4",
+      github: "https://github.com/sachinaryan912"
+    },
+    {
+      name: "Sauryadeep Roy",
+      role: "Software Engineer",
+      avatar: "https://avatars.githubusercontent.com/u/75126902?v=4",
+      github: "https://github.com/Sauryadeep200"
+    }
+  ];
